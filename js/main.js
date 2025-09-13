c4cfab983257ccf483c3403796c171a8f9999466
Meryem L. <82731366+L-Meryem@users.noreply.github.com>
2025-09-13 19:13:10 -0400

    Add sound, legends, total wins and losses, css styling

--- js/main.js
+++ js/main.js
@@ -7,22 +7,31 @@ const min_bet = document.querySelector('#min_bet');
 max_bet.addEventListener('click', () => spinner(50));
 min_bet.addEventListener('click', () => spinner(5));
 
+var winAudio = new Audio('assets/winSound.mp3');
+var noBalanceAudio = new Audio('assets/noBalance.mp3');
 
+const win = document.querySelector('#win');
 
 function spinner(bet) {
     let balance = Number(document.querySelector('#balance').innerText);
 
     //Check if enough balance
     if (balance < bet) {
-        const win = document.querySelector('#win');
-        win.innerText = 'NO BALANCE';
+        win.innerText = 'NOT ENOUGH BALANCE';
         win.style.color = 'red';
-        setTimeout(() => document.querySelector('#win').innerText = '', 1000);
+        noBalanceAudio.play();
+        setTimeout(() => {
+            win.innerText = '';
+        }, 2000);
         return;
     }
 
     // sub from balance
     balance = balance - bet;
+    win.innerText = '-' + bet;
+    win.style.color = 'red';
+    const loss = document.querySelector('#loss')
+    loss.innerHTML = Number(loss.innerHTML) + bet;
 
     //Spin, get reels in a [] and check if they align
     const reel = document.querySelectorAll(".reel");
@@ -32,7 +41,6 @@ function spinner(bet) {
         let item = spin();
         reelsAlign.push(item);
         reel.innerText = item;
-        reel.classList.add('.reel');
     });
 
     // Do the reels line up
@@ -40,28 +48,32 @@ function spinner(bet) {
 
     //Payout
     if (bet != false) {
-        
-        console.log('balance before = '+ balance);
         balance = balance + bet;
-        console.log('bet = '+bet);
-        console.log('balance after = '+ balance);
+        win.style.color = 'green';
+        document.querySelector('#win').innerText = '+' + bet;
+        winAudio.play();
+        const lastWin = document.querySelector('#lastWin')
+        lastWin.innerHTML = Number(lastWin.innerHTML) + bet;
 
-        document.querySelector('#win').innerText = 'WIN! +' + bet;
         setTimeout(() => {
             document.querySelector('#win').innerText = '';
-        }, 1000);
+        }, 1500);
     }
 
     document.querySelector('#balance').innerText = balance;
+    // Reset the animation
+    win.style.animation = "none";
+    win.offsetHeight;// A fix, not sure why but it works
+    win.style.animation = "upYouGo 1s linear forwards";
 }
 
 function allReelsAlign(arr, bet) {
     if (arr.every(item => item === arr[0])) {
-        if (arr[0] === 'star') bet *= 20;
-        if (arr[0] === 'moon') bet *= 10;
-        if (arr[0] === 'leaf') bet *= 6;
-        if (arr[0] === 'mouse') bet *= 3;
-        if (arr[0] === 'spaghetti') bet *= 2;
+        if (arr[0] === '🧞‍♀️') bet *= 20;
+        if (arr[0] === '🦄') bet *= 10;
+        if (arr[0] === '🦧') bet *= 6;
+        if (arr[0] === '🐸') bet *= 3;
+        if (arr[0] === '🗿') bet *= 2;
         return bet;
     }
     else return false;
@@ -71,15 +83,15 @@ function allReelsAlign(arr, bet) {
 function spin() {
     const random = Math.floor(Math.random() * 100 + 1);
     if (random >= 95) // 5/100 chance to appear
-        return 'star'; //20× bet 
+        return '🧞‍♀️'; //20× bet 
     if (random <= 94 && random >= 85)
-        return 'moon'; //10× bet
+        return '🦄'; //10× bet
     if (random <= 84 && random >= 65)
-        return 'leaf'; //6× bet
+        return '🦧'; //6× bet
     if (random <= 64 && random >= 40)
-        return 'mouse'; //3× bet
+        return '🐸'; //3× bet
     if (random <= 39 && random >= 1) // ~40/100 chance to appear
-        return 'Spaghetti'; //2× bet
+        return '🗿'; //2× bet
 }
 
 
